7b527bbc77a4cc8b057193f7bd9b9f65adde06f6
Tiffany J Brown <tiffany.brownjc@gmail.com>
2018-09-10 14:48:22 -0400

    created pages

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 import "./style.css";
 import Welcome from "../Welcome";
-//import Date from "../Date";
+import CurrentMeds from "../CurrentMeds";
 import UserInput from "../UserInput";
 import MedInput from "../MedInput";
-
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      currentPage: "Welcome",
-      userInfo: [{ 
+      currentPanel: "Welcome",
+      userInfo: [{
         name: " ",
         dob: " ",
       }],
@@ -27,7 +26,7 @@ class App extends Component {
       ]
     }
     console.log(this.state.userInfo, this.state.medsList);
-  }  
+  }
   newMedList = (newItem) => {
     this.setState(prevState => {
       const newMeds = [...prevState.medsList];
@@ -46,13 +45,34 @@ class App extends Component {
       }
     })
   }
+  welcome = () => {
+    this.setState({
+      currentPanel: "welcome",
+    })
+  }
+  medInput = () => {
+    this.setState({
+      currentPanel: "medInput",
+    })
+  }
+  userInput = () => {
+    this.setState({
+      currentPanel: "userInput",
+    })
+  }
+  return = () => {
+    this.setState({
+      currentPanel: "welcome",
+    })
+  }
+
   render() {
     console.log(this.state.medsList);
     console.log(this.state.userInfo);
-    
+
     return (
       <div className="App">
-        <h1>Your Medicine Cabinet</h1>
+        {/* <h1>Your Medicine Cabinet</h1>
         {this.state.medsList.map(med => {
           return (<h3 key={med.name + med.dose + med.quantity + med.directions + med.time}>
             {med.name} {med.dose} {med.quantity} {med.directions} {med.time}</h3>);
@@ -60,10 +80,42 @@ class App extends Component {
         <h1>Your Info</h1>  
         {this.state.userInfo.map(info => {
           return (<h3 key={info.name + info.dob}>{info.name} {info.dob}</h3>)
-        })}     
-        <MedInput newMedList={this.newMedList} newList={this.state.medsList} />
-        <UserInput newUserInfo={this.newUserInfo} newPatient={this.state.userInfo} />
-        <Welcome />
+        })}     */}
+        {/* <Welcome /> */}
+        {this.state.currentPanel === "Welcome" && (
+          <div>
+            <Welcome
+            // medsList={this.state.medInput}
+            // userInfo={this.state.UserInput}
+            />
+            <div className="nav">
+              <button className="goToMed" onClick={this.medInput}>Input Med Info</button>
+              <button className="goToUser" onClick={this.userInput}>Input User Info</button>
+            </div>
+          </div>
+        )}
+        {/* <div className="nav">
+          <button className="goToMed" onClick={this.medInput}>Input Med Info</button>
+          <button className="goToUser" onClick={this.userInput}>Input User Info</button>
+        </div>  */}
+
+        {/* <UserInput newUserInfo={this.newUserInfo} newPatient={this.state.userInfo} /> */}
+        {this.state.currentPanel === "userInput" && (
+          <UserInput
+            userInfo={this.state.userInput}
+            return={this.return}
+          />
+        )}
+        {/* <MedInput newMedList={this.newMedList} newList={this.state.medsList} /> */}
+        {this.state.currentPanel === "medInput" && (
+          <MedInput
+            medsList={this.state.medInput}
+            return={this.return}
+          />
+        )}
+
+        {this.state.currentPanel === "CurrentMeds" && <CurrentMeds />}
+
       </div>
     )
   }
---
+++ src/CurrentMeds/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class CurrentMeds extends Component {
+    render() {
+        return (
+            <div className="currentList">
+                <h1>Your Current Medications</h1>
+            </div>
+        )
+    }
+}
+export default CurrentMeds;
